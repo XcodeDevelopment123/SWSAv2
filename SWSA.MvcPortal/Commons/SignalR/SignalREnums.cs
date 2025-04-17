206db7d242460d3b39ce93c870b81088af0d54c3
Ong Sen Hong <130441206+XcodeDevelopment123@users.noreply.github.com>
2025-04-17 09:33:36 +0800

    [master] add signal R

---
+++ SWSA.MvcPortal/Commons/SignalR/SignalREnums.cs
@@ -0,0 +1,13 @@
+﻿namespace SWSA.MvcPortal.Commons.SignalR;
+
+
+public enum SignalREventType
+{
+    AssignmentDueSoon,
+}
+
+public enum SignalRGroupType
+{
+    NotificationGroup,
+}
+
---
+++ SWSA.MvcPortal/Commons/SignalR/SignalRMessage.cs
@@ -0,0 +1,36 @@
+﻿using Newtonsoft.Json;
+
+namespace SWSA.MvcPortal.Commons.SignalR;
+
+
+public class SignalRMessage<T>
+{
+    public string Message { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public string? StaffId { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+    public T? Data { get; set; }
+
+    public SignalRMessage(string message)
+    {
+        Message = message;
+    }
+
+    public SignalRMessage(string message, string? staffId, T? data)
+    {
+        Message = message;
+        StaffId = staffId;
+        Data = data;
+    }
+
+    public string ToJson()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+
+    //public async Task SendPermissionChangeToUser(IHubContext<NotificationHub> hubContext, int userId)
+    //{
+    //    var eventName = SignalREventType.PermissionChange.ToString();
+    //    await hubContext.Clients.User(userId.ToString()).SendAsync(eventName, this.ToJson());
+    //}
+}
