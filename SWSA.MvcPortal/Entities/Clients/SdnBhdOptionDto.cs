d59a140cea71096f6959a963d7f6b58b6952a3b6
mr1199227 <mr1199227@gmail.com>
2025-11-16 09:39:18 +0800

    [mr] -update function Reminder B31 link -add dto SdnBhdOptionDto ,function GetAllSdnBhdOptionsAsync

---
+++ SWSA.MvcPortal/Entities/Clients/SdnBhdOptionDto.cs
@@ -0,0 +1,9 @@
+﻿namespace SWSA.MvcPortal.Entities.Clients
+{
+    public class SdnBhdOptionDto
+    {
+        public int Id { get; set; }                 // ClientId
+        public string Name { get; set; } = "";      // 公司名
+        public DateTime? IncorporationDate { get; set; }  // 成立日期
+    }
+}
