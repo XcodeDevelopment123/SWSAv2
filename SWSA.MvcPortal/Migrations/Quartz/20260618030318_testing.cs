96f576111ce9bc6b55be07404ab242f1bed344c6
ryoku978-lab <ryoku978@gmail.com>
2026-06-18 11:05:00 +0800

    11

---
+++ SWSA.MvcPortal/Migrations/Quartz/20260618030318_testing.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SWSA.MvcPortal.Migrations.Quartz
+{
+    /// <inheritdoc />
+    public partial class testing : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "AmountTaxPayable",
+                table: "BP34",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FinalTaxComplete",
+                table: "BP34",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InvoiceAmount",
+                table: "BP34",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InvoiceDate",
+                table: "BP34",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "ReviewTaxComplete",
+                table: "BP34",
+                type: "nvarchar(max)",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "InvoiceAmount",
+                table: "BP33",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AmountTaxPayable",
+                table: "BP34");
+
+            migrationBuilder.DropColumn(
+                name: "FinalTaxComplete",
+                table: "BP34");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceAmount",
+                table: "BP34");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceDate",
+                table: "BP34");
+
+            migrationBuilder.DropColumn(
+                name: "ReviewTaxComplete",
+                table: "BP34");
+
+            migrationBuilder.DropColumn(
+                name: "InvoiceAmount",
+                table: "BP33");
+        }
+    }
+}
